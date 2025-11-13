50d25aef26b5636aa6c77449cce72661a8e5f544
Anna Khit <annakhit02@mail.ru>
2025-11-13 11:04:05 +0300

    Apply clang-format Mozilla style

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,10 +3,12 @@
 
 // Main function demonstrating basic input/output - UPDATED!
 // This program asks for user name and prints greeting - CHANGED!
-int main() {
-    std::string name;
-    std::cout << "Enter your name: ";
-    std::cin >> name;
-    std::cout << "Hello world from " << name << std::endl;
-    return 0;  // Program executed successfully
+int
+main()
+{
+  std::string name;
+  std::cout << "Enter your name: ";
+  std::cin >> name;
+  std::cout << "Hello world from " << name << std::endl;
+  return 0; // Program executed successfully
 }

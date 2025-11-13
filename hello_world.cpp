acce1f80ef8e17000438209b054b0424dedeea7f
Anna Khit <annakhit02@mail.ru>
2025-11-13 10:38:42 +0300

    Add code comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <string>
 
+// Main function demonstrating basic input/output
 int main() {
     std::string name;
     std::cout << "Enter your name: ";
     std::cin >> name;
     std::cout << "Hello world from " << name << std::endl;
-    return 0;
+    return 0;  // Program executed successfully
 }
